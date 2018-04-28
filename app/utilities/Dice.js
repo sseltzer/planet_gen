4ba371e37eca0520137fee47a3fd7d009830e155
Charles Heller <chheller@uark.edu>
2018-04-27 21:26:44 -0500

    unrefactor

--- app/utilities/Dice.js
+++ app/utilities/Dice.js
@@ -1,16 +1,42 @@
 const seedrandom = require('seedrandom');
 
-
-
 class Dice {
-  constructor(dice = []) {
-    this.dice = dice
+
+  constructor(seed, state = false) {
+    this.seed = seed;
+    this.state = state;
+    this.seedrandom = Math.seedrandom;
+    this.saveable = seedrandom(seed, {state});
   }
 
-  roll(rolls) {
-   return [...this.dice].map(die => die.roll(rolls)).reduce((prev,curr)=> prev + curr);
+  roll() {
+    return this;
+  }
+
+  one() {
+    this.rolls = 1;
+    return this;
+  }
+
+  two() {
+    this.rolls = 2;
+    return this;
+  }
+
+  three() {
+    this.rolls = 3;
+    return this;
+  }
+
+  d6() {
+    let roll = this.rollDie(6);
+    this.one();
+    return roll;
+  }
+
+  rollDie(sides) {
+    return [...Array(this.rolls)].map(i => Math.floor(this.saveable() * sides) + 1).reduce((a, b) => a + b);
   }
 }
-
 
 module.exports = Dice;
